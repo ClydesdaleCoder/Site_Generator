1aa71c7a125f2b70b8c6bcac4399854dbf91aaf4
ClydesdaleCoder <russellanthonygardner@gmail.com>
2025-05-28 09:10:27 -0700

    finally finished the split images function 5/28/25

--- src/split_images_link.py
+++ src/split_images_link.py
@@ -11,5 +11,22 @@ def split_nodes_image(old_nodes):
             split_nodes.append(old)
 
         else:
-        
-        
+            current_text = old.text
+            for p in processed:
+                image_section = "!["+ p[0] + "("+ p[1] + ")"
+                
+                around_image = current_text.split(image_section,1)
+                
+                if around_image[0] != "":
+                    
+                    split_nodes.append(TextNode(around_image[0],TextType.TEXT))
+
+                split_nodes.append(TextNode(p[0],TextType.IMAGE,p[1]))
+
+                remainder = around_image[1]
+                
+                current_text = remainder
+
+            if current_text != "":
+                split_nodes.append(TextNode(current_text,TextType.TEXT))
+
